f06315382374fbfb712c2f126541cdb2a6a24732
AishwaryaA <aishwaryaa@nedbank.co.za>
2024-05-30 18:13:01 +0530

    updating the image to show db data on browser

--- index.js
+++ index.js
@@ -11,15 +11,26 @@ mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.get('/clients', async (req, res) => {
-    console.log('initializing service');
-    const clients = await mongoose.connection.db.collection('clients').find().toArray();
-    console.log('line 20 ran');
-    res.json(clients);
+app.get('/', async (req, res) => {
+    try {
+        const clients = await mongoose.connection.db.collection('clients').find().toArray();
+        let clientList = clients.map(client => `<li>${client.name} - ${client.email}</li>`).join('');
+        res.send(`
+            <html>
+                <head>
+                    <title>Client List</title>
+                </head>
+                <body>
+                    <h1>Client List</h1>
+                    <ul>
+                        ${clientList}
+                    </ul>
+                </body>
+            </html>
+        `);
+    } catch (err) {
+        res.status(500).send('Error fetching clients');
+    }
 });
 
 app.listen(port, () => {
